574b7717a91527a5a703c1f69d5f8e22d26d2620
Roman Malcev <roman-malcev@inbox.ru>
2020-04-07 00:09:22 +0300

    1.3-data-types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,24 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+  // код для задачи №1 писать здесь
+  let bodyDebt = amount - contribution;
+  let numberMonths = Math.round((date - new Date()) / 86400000 / 30.41667); 
+  let totalAmount = (bodyDebt * (percent / 100 / 12 + percent / 100 / 12 / (Math.pow(1 + percent / 100 / 12, numberMonths) - 1))) * numberMonths;
+  totalAmount = (Math.round(totalAmount * 100)) / 100;
+  
+  console.log(totalAmount);
+  return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+  // код для задачи №2 писать здесь
+  let greeting;
+
+  if (name == null || name == "" || name == undefined) {
+    greeting = "Привет, мир! Меня зовут Аноним";
+  } else greeting = `Привет, мир! Меня зовут ${name}`;
+  
+  return greeting;
+  console.log(greeting);
 }
